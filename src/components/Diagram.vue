b6b90e72b3282fa1c43fd7f583c87d6934e16fa3
Stephen Rich <stephen@bri-associates.com>
2024-01-16 17:19:56 -0800

    groupTemplateMap fail

--- src/components/Diagram.vue
+++ src/components/Diagram.vue
@@ -22,6 +22,44 @@ onMounted(function() {
   myDiagram.addDiagramListener("ExternalObjectsDropped", e => emitter("ExternalObjectsDropped", e))
   myDiagram.addDiagramListener("SelectionMoved", e => emitter("SelectionMoved", e))
 
+  myDiagram.groupTemplateMap.add("table", 
+				$(go.Group, "Auto", 									// New group sized automatically 
+					{
+						ungroupable: true,								// Can not add this group into a parent group 
+						layout: $(go.GridLayout,{   					// New sub-grid layout 
+							wrappingColumn: 	1,						// One column without wrapping 
+							alignment: 			go.GridLayout.Position,	// Place by position method 
+							cellSize: 			new go.Size(1, 1),		// Grid sizing 
+							//spacing: 			new go.Size(0, 0),      // Padding 
+						}),
+						computesBoundsAfterDrag: true,					// Don't recompute bounds while dragging is occurring 
+						layer: 					"background",			// The group is just in the background.  Doesn't interact with anything 
+						avoidable: 				false,					// No reason to go around the group (links go through it)
+						deletable: 				false,					// Can't delete it 
+						copyable: 				false,					// Can't copy the group ... not logical spock 
+						movable:				false,
+					},	
+					// Create the nested objects within the group 
+					$(go.Panel, "Vertical",								
+						{
+							//background: "green", 						//TODO: gid rid of me when done tweaking layout & margins
+							alignment: go.Spot.Top,
+							stretch: go.GraphObject.Vertical,
+							//width:	"100%",
+							//stretch: go.GraphObject.Horizontal 			// Doesn't seem to do anything
+						},
+						$(go.Placeholder,
+							{ 
+								padding: 0,
+								stretch: go.GraphObject.Horizontal      // Doesn't seem to do anything
+	
+							}
+						),
+					),
+				)
+			);
+
+
   myDiagram.nodeTemplate =
     $(go.Node, "Auto",
       $(go.Shape,
